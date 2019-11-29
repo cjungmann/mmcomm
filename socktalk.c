14005b99d9f017128d5ed8342673bdb8642499a5
Chuck Jungmann <chuckj60@gmail.com>
2019-11-29 16:16:41 -0600

    Reordered functions so prerequisite functions are defined first. Added a stk_vsend_line that takes a va_list argument to support send_recv in one function.

--- socktalk.c
+++ socktalk.c
@@ -3,89 +3,6 @@
 
 #include "socktalk.h"
 
-
-size_t stk_sock_talker(const struct _stalker* talker, const void *data, int data_len)
-{
-   return send(talker->socket_handle, (void*)data, data_len, 0);
-}
-
-size_t stk_ssl_talker(const struct _stalker* talker, const void *data, int data_len)
-{
-   return SSL_write(talker->ssl_handle, data, data_len);
-}
-
-size_t stk_sock_reader(const struct _stalker* talker, void *buffer, int buff_len)
-{
-   return recv(talker->socket_handle, buffer, buff_len, 0);
-}
-
-size_t stk_ssl_reader(const struct _stalker* talker, void *buffer, int buff_len)
-{
-   return SSL_read(talker->ssl_handle, buffer, buff_len);
-}
-
-
-void init_ssl_talker(struct _stalker* talker, SSL* ssl)
-{
-   memset(talker, 0, sizeof(struct _stalker));
-   talker->ssl_handle = (void*)ssl;
-   talker->writer = stk_ssl_talker;
-   talker->reader = stk_ssl_reader;
-}
-
-void init_sock_talker(struct _stalker* talker, int socket)
-{
-   memset(talker, 0, sizeof(struct _stalker));
-   talker->socket_handle = socket;
-   talker->writer = stk_sock_talker;
-   talker->reader = stk_sock_reader;
-}
-
-/**
- * @brief Write string of const char* arguments to socket or ssl socket, finish with "\r\n";
- *
- * This is a variable-argument function, with the _talker argument first,
- * followed by const char* arguments, terminated with a NULL argument to
- * indicate the end of the list.
- *
- * No spaces will be added between argument strings, but a final "\r\n"
- * will be sent upon encountering the terminating NULL.
- */
-size_t stk_send_line(const struct _stalker* talker, ...)
-{
-   size_t bytes_sent, total_bytes = 0;
-   size_t bite_len;
-   va_list ap;
-   va_start(ap, talker);
-
-   const char *bite = va_arg(ap, const char*);
-   while (bite)
-   {
-      bite_len = strlen(bite);
-      total_bytes += bytes_sent = (*talker->writer)(talker, bite, bite_len);
-      if (bytes_sent != bite_len)
-         fprintf(stderr, "Socket talker failed to write complete contents of string.\n");
-
-      bite = va_arg(ap, const char*);
-   }
-
-   total_bytes += bytes_sent = (*talker->writer)(talker, "\r\n", 2);
-
-   return total_bytes;
-}
-
-/**
- * @brief Read from server using current communication protocol.  Add \0 to end, if room.
- */
-size_t stk_recv_line(const struct _stalker* talker, void* buffer, int buff_len)
-{
-   size_t bytes_read = (*talker->reader)(talker, buffer, buff_len);
-   if (bytes_read+1 < buff_len)
-      ((char*)buffer)[bytes_read] = '\0';
-   return bytes_read;
-}
-
-
 int walk_status_reply(const char *str, int *status, const char** line, int *line_len)
 {
    int i_status = 0;
@@ -158,6 +75,164 @@ void dump_status_reply(const char *buffer, int buffer_len)
             break;
       }
    }
+}
+
+int log_status_reply_errors(const char *buffer, int buffer_len)
+{
+   int errors = 0;
+   const char *ptr = buffer;
+   const char *end = buffer  + buffer_len;
+
+   int advance_chars;
+
+   // walk_status_reply() output parameter variables
+   int status;
+   const char *line;
+   int line_len;
+   
+   while (ptr < end && *ptr)
+   {
+      advance_chars = walk_status_reply(buffer, &status, &line, &line_len);
+      switch(advance_chars)
+      {
+         case -1:
+            fprintf(stderr, "Error processing replys from \"%s\"\n", buffer);
+         case 0:
+            ptr = end;  // Flag to break outer loop
+            break;
+         default:
+            if (status >= 400)
+            {
+               printf("Error (%d) : %.*s.\n", status, line_len, line);
+               ++errors;
+            }
+            ptr += advance_chars;
+            break;
+      }
+   }
+   return errors;
+}
+
+size_t stk_sock_talker(const struct _stalker* talker, const void *data, int data_len)
+{
+   return send(talker->socket_handle, (void*)data, data_len, 0);
+}
+
+size_t stk_ssl_talker(const struct _stalker* talker, const void *data, int data_len)
+{
+   return SSL_write(talker->ssl_handle, data, data_len);
+}
+
+size_t stk_sock_reader(const struct _stalker* talker, void *buffer, int buff_len)
+{
+   return recv(talker->socket_handle, buffer, buff_len, 0);
+}
+
+size_t stk_ssl_reader(const struct _stalker* talker, void *buffer, int buff_len)
+{
+   return SSL_read(talker->ssl_handle, buffer, buff_len);
+}
+
+
+void init_ssl_talker(struct _stalker* talker, SSL* ssl)
+{
+   memset(talker, 0, sizeof(struct _stalker));
+   talker->ssl_handle = (void*)ssl;
+   talker->writer = stk_ssl_talker;
+   talker->reader = stk_ssl_reader;
+}
+
+void init_sock_talker(struct _stalker* talker, int socket)
+{
+   memset(talker, 0, sizeof(struct _stalker));
+   talker->socket_handle = socket;
+   talker->writer = stk_sock_talker;
+   talker->reader = stk_sock_reader;
+}
+
+size_t stk_vsend_line(const struct _stalker* talker, va_list args)
+{
+   size_t bytes_sent, total_bytes = 0;
+   size_t bite_len;
+
+   va_list args_copy;
+   va_copy(args_copy, args);
+
+   const char *bite = va_arg(args_copy, const char*);
+   while (bite)
+   {
+      bite_len = strlen(bite);
+      total_bytes += bytes_sent = (*talker->writer)(talker, bite, bite_len);
+      if (bytes_sent != bite_len)
+         fprintf(stderr, "Socket talker failed to write complete contents of string.\n");
+
+      bite = va_arg(args_copy, const char*);
+   }
+
+   va_end(args_copy);
+
+   total_bytes += bytes_sent = (*talker->writer)(talker, "\r\n", 2);
+
+   return total_bytes;
+}
+
+/**
+ * @brief Write string of const char* arguments to socket or ssl socket, finish with "\r\n";
+ *
+ * This is a variable-argument function, with the _talker argument first,
+ * followed by const char* arguments, terminated with a NULL argument to
+ * indicate the end of the list.
+ *
+ * No spaces will be added between argument strings, but a final "\r\n"
+ * will be sent upon encountering the terminating NULL.
+ */
+size_t stk_send_line(const struct _stalker* talker, ...)
+{
+   size_t bytes_sent, total_bytes = 0;
+   size_t bite_len;
+   va_list ap;
+   va_start(ap, talker);
+
+   const char *bite = va_arg(ap, const char*);
+   while (bite)
+   {
+      bite_len = strlen(bite);
+      total_bytes += bytes_sent = (*talker->writer)(talker, bite, bite_len);
+      if (bytes_sent != bite_len)
+         fprintf(stderr, "Socket talker failed to write complete contents of string.\n");
+
+      bite = va_arg(ap, const char*);
+   }
+
+   va_end(ap);
+
+   total_bytes += bytes_sent = (*talker->writer)(talker, "\r\n", 2);
+
+   return total_bytes;
+}
+
+/**
+ * @brief Read from server using current communication protocol.  Add \0 to end, if room.
+ */
+size_t stk_recv_line(const struct _stalker* talker, void* buffer, int buff_len)
+{
+   size_t bytes_read = (*talker->reader)(talker, buffer, buff_len);
+   if (bytes_read+1 < buff_len)
+      ((char*)buffer)[bytes_read] = '\0';
+   return bytes_read;
+}
+
+int stk_send_recv_line(const struct _stalker *talker, ...)
+{
+   char buffer[1000];
+
+   va_list args;
+   va_start(args, talker);
+   stk_vsend_line(talker, args);
+   va_end(args);
+
+   stk_recv_line(talker, buffer, sizeof(buffer));
+   return 0 == log_status_reply_errors(buffer, sizeof(buffer));
 }
 
 int seek_status_message(const struct _status_line* sl, const char *value)
--- socktalk.h
+++ socktalk.h
@@ -14,6 +14,28 @@ struct _stalker;
 typedef size_t (*SockWriter)(const struct _stalker*, const void *data, int data_len);
 typedef size_t (*SockReader)(const struct _stalker*, void *buffer, int buff_len);
 
+
+/**
+ * @brief Read *str to the next \r\n, setting output parameters *status **line, and *line_len.
+ *
+ * Use this function to parse the data returned from a SMTP server.  Use this
+ * function to walk through the status and print or save the reply messages.
+ */
+int walk_status_reply(const char *str, int *status, const char** line, int *line_len);
+
+/**
+ * @brief Debugging function that also illustrates simple use of walk_status_reply().
+ */
+void dump_status_reply(const char *buffer, int buffer_len);
+
+/**
+ * @brief Searches reply buffer for error messages, which are logged.
+ *
+ * @return Number of errors encountered.  (0 for no errors, duh.)
+ */
+int log_status_reply_errors(const char *buffer, int buffer_len);
+
+
 size_t stk_sock_talker(const struct _stalker* talker, const void *data, int data_len);
 size_t stk_ssl_talker(const struct _stalker* talker, const void *data, int data_len);
 
@@ -47,22 +69,12 @@ void init_sock_talker(struct _stalker* talker, int socket);
 /**
  * Functions that actually read or write using the STalker object.
  */
+size_t stk_vsend_line(const struct _stalker* talker, va_list args);
 size_t stk_send_line(const struct _stalker* talker, ...);
 size_t stk_recv_line(const struct _stalker* talker, void *buffer, int buff_len);
+/** Send text like std_send_line, read and check response before returning. */
+int stk_send_recv_line(const struct _stalker *talker, ...);
 
-
-/**
- * @brief Read *str to the next \r\n, setting output parameters *status **line, and *line_len.
- *
- * Use this function to parse the data returned from a SMTP server.  Use this
- * function to walk through the status and print or save the reply messages.
- */
-int walk_status_reply(const char *str, int *status, const char** line, int *line_len);
-
-/**
- * @brief Debugging function that also illustrates simple use of walk_status_reply().
- */
-void dump_status_reply(const char *buffer, int buffer_len);
 
 /**
  * @brief Given a chain of Status_Line, return 1 if a given message can be found, 0 otherwise.
